25cfc5091e0b147a62bbc3f036e286e798bd7d29
John-Colvin <john.loughran.colvin@gmail.com>
2014-10-26 22:26:22 +0000

    more implementation

--- wrap.d
+++ wrap.d
@@ -350,6 +350,8 @@ auto createCommandQueue(cl_context context, cl_device_id device, cl_command_queu
             );
 }
 
+enum OUT_OF_ORDER_EXEC = CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE;
+
 struct CLProgram
 {
     cl_program id;
@@ -420,148 +422,14 @@ cl_program buildProgram(cl_program program, cl_device_id[] devices = null, const
     return program;
 }
 
-
-/**
- * Thread-local status flag. Just for convenience to not have to declare a new one in
- * every function that needs one.
- */
-cl_int status;
-
-//sometimes no need for events that check partial completion for
-//pipes, OpenCL 2.0 has pipes built in.
-
-//Ranges to work over are *arguments*, not queueing parameters.
-//They could be stored with memory objects for simple cases.
-//In some cases they are genuinely seperate from memory though.
-//represented by a set of min-max pairs. max CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS
-//pairs.
-//The generalisation of this is a set of D ranges.
-
-//General case of events
-
-//special cases: openCL kernels, native function.
-
-//what defines something as enqueueable? must define typeList to be called with
-// must define number of dimensions it is parallelised over
-
-// currently just represents preperations for a task, doesn't have
-// anything to say once you've enqueued it.
-struct Task(alias queueable, dependencies = DependsOn!())
-    if(is(dependencies : DependsOn!X, X ...))
-{
-    alias work = queueable;
-
-    alias dependencyTypes = dependencies;
-
-    static if(isNativeFunction!queueable)
-    {
-        alias ArgTypes = ParameterTypeTuple!(queueable);
-    }
-    else static if(isCLKernel!queueable)
-    {
-        alias ArgsTypes = queueable.ArgTypes;
-    }
-    else static assert(false, 
-            "Task type \"" ~ typeof(queueable).stringof ~ "\" not supported");
-
-    ArgTypes defaultArgs;
-
-    cl_command_queue[] defaultQueues;
-
-    size_t[2][work.nParallelDims][] globalRange;
-    size_t[work.nParallelDims][] localSize;
-    
-    void setArgs(Args...)(Args args)
-    {
-        defaultArgs = args;
-    }
-    
-    auto instance(size_t queueIndex)
-    {
-        return TaskInstance(defaultQueues[queueIndex],
-                globalRange[queueIndex],
-                localSize[queueIndex],
-                defaultArgs);
-    }
-}
-
-private template ExtractTypes(Args ...)
-{
-    static if(Args.length == 0)
-        alias ExtractTypes = TT!();
-    else
-    {
-        static if(is(typeof(Args[0])))
-            alias T = typeof(Args[0]);
-        else
-            alias T = Args[0];
-        alias ExtractTypes = TT!(T, ExtractTypes!(Args[1..$]));
-    }
-}
-
-struct DependsOn(Tasks ...)
-{
-    alias types = ExtractTypes!TaskTs;
-}
-
-struct TaskInstance(TaskT)
-{
-    //dependencies could be arrays of dependencies, for
-    //variable length things
-    cl_event[TaskT.dependencies.length] blockers;
-
-    size_t[2][TaskT.work.nParallelDims] globalRange;
-    size_t[TaskT.work.nParallelDims] localSize;
-
-    TaskT.ArgTypes args;
-
-    cl_command_queue queue;
-
-    this(cl_command_queue queue,
-            size_t[2][TaskT.work.nParallelDims] globalRange,
-            size_t[TaskT.work.nParallelDims] localSize,
-            TaskT.ArgTypes args)
-    {
-        this.args = args;
-        this.localSize = localSize;
-        this.globalRange = globalRange;
-        this.queue = queue;
-    }
-
-    cl_event opCall()
-    {
-        return enqueue();
-    }
-
-    cl_event enqueue()
-    {
-        cl_event ev;
-        static if(isNativeFunction!(TaskT.work))
-        {
-            static assert(false, "not implemented");
-        }
-        else static if(isCLKernel!(TaskT.work))
-        {
-            clSetKernelArgs(TaskT.queueable, args);
-            clEnqueueCLKernel(queue, TaskT.work,
-                    globalRange[].map!"a[1] - a[0]".array,
-                    globalRange[].map!"a[0]".array,
-                    localSize[],
-                    blockers[],
-                    &ev);
-        }
-        else static assert(false, 
-                "Task type \"" ~ typeof(queueable).stringof ~ "\" not supported");
-        return ev;
-    }
-}
-
-struct Kernel(ArgTypes_ ...)
+struct Kernel(uint nDims, ArgTypes_ ...)
 {
     cl_kernel id;
     alias id this;
  
-    alias ArgTypes = ArgsTypes_;
+    alias ArgTypes = ArgTypes_;
+
+    enum nParallelDims = nDims;
 
     this(cl_kernel id)
     {
@@ -572,105 +440,13 @@ struct Kernel(ArgTypes_ ...)
     {
         return .getInfo!(clGetKernelInfo, flag, KernelInfoEnums)(id);
     }
-
 }
 
-auto enqueue(Task)(cl_command_queue queue, Task task, RangeInfo ri)
-    if(isOpenCLTask!Task)
-{
-    cl_event event;
-    size_t[
-    clEnqueueCLKernel(queue, task.work,  task.dependsOn, &event);
-    return event;
-}
 
-//looks up RangeInfo in the task
-auto enqueue(Task)(cl_command_queue queue, Task task)
-    if(isOpenCLTask!Task)
-{
-    cl_event event;
-    
-    return event;
-}
-
-auto enqueue(Task)(cl_command_queue queue, Task task)
-    if(isNativeTask!Task)
-{
-    cl_event event;
+/**
+ * Thread-local status flag. Just for convenience to not have to declare a new one in
+ * every function that needs one.
+ */
+cl_int status;
 
 
-}
-
-//Directed Acyclic Graph
-//Can be implicitly created through dependsOn.
-//Jobs have to be scheduled in topological order as they can't
-//wait for a non-existant event (dummy events???)
-//Should be able to do transitive reduction at any point to
-//clean it up, then topological sort.
-
-//What levels are required here:
-
-//    default setup for a task: queues, ranges, arguments.
-
-//    inidividual instantiation of a task: override defaults, has an event associated with it
-//    and can form a part of the DAG
-
-// The DAG must be creatable on the fly: in the general case it is infinite (e.g. non-terminating
-// loop enqueueing work).
-
-// Can find sub-graphs that can treated as a single event. Essentially we have a graph of graphs,
-// where the granualarity is decided by what?
-// A subgraph can be modeled as a single event iff all inputs are required to start and all
-// outputs are simultaneously blocking i.e. nothing that depends on any part of the subgraph can
-// start unless all parts of the subgraph are complete
-
-//Dummy events: essentially requires an internal 
-
-unittest
-{
-    auto queue = makeAQueue();
-    Task!someKernel task1;
-    Task!someOtherKernel task2;
-
-    // expresses that any instance of task2 must depend on the previous completion of an
-    // instace of task1. Would be nicer as a compile-time thing
-    task2.dependsOn(task1);
-    
-    //alternatively, could do something like this:
-    /+
-    Task!(someKernel, DependsOn!(task1)) task2;
-    +/
-
-    //setting these also adds the queue to task1.
-    task1.globalRange(queue) = [[0, 100], [3, 7]];
-    task1.localSize(queue) = [20, 1];
-
-    task2.globalRange(queue) = [[0, 100], [0, 10]];
-    task2.localSize(queue) = [20, 1];
-
-    task2.setArgs(arg3);
-
-    //enqueues an instance of task1 on all queues associated with task1
-    auto t1Ev = task1(arg1, arg2);
-
-    auto task2i = task2.instance();
-
-    //override the default args
-    task2i.setArgs(arg3New);
-    
-    //Error: haven't set the dependency up
-    //task2i.enqueue();
-
-    task2i.dependsOn(t1Ev);
-
-    //checks that dependency is set, everything OK.
-    //does a lookup in the tasks queue table to find range info for this queue
-    auto t2Ev = queue.enqueue(task2);
-
-    auto someMemory = new Type[n];
-    auto results = queue.clRead(resBuffer, someMemory, true, 0, [t2Ev]);
-
-    //continue
-}
-
-//Can't help feeling Haskell would have something lovely for all this....
